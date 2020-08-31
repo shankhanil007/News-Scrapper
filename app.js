c2d5290c69ff2c958c9417e882f0437ae5157a02
Shankhanil Borthakur <shankhanilborthakur@gmail.com>
2020-09-01 03:21:15 +0530

    Basic Functionality done

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require("express");
 var bodyParser = require("body-parser");
 const request = require("request");
 var app = express();
+var async = require("async");
 const puppeteer = require("puppeteer");
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", function (req, res) {
 
@@ -18,14 +20,98 @@ app.get("/", function (req, res) {
     var URL_data = await page.evaluate(() =>
       Array.from(document.querySelector('ul.itg-listing').getElementsByTagName("li"))
         .map(compact => ({
-          url: url + compact.querySelector("a").getAttribute("href")
+          url: "https://www.indiatoday.in" + compact.querySelector("a").getAttribute("href")
         }))
     )
+
+    // url = URL_data[0].url;
+    // console.log(url);
+    // (async () => {
+    //   const browser = await puppeteer.launch();
+    //   const page = await browser.newPage();
+    //   await page.goto(url);
+    //   //   await page.screenshot({path: 'example.png'});
+
+    //   var news_data = await page.evaluate(() => ({
+    //     details: document.querySelector("h1").innerText
+    //   })
+    //   )
+    //   console.log(news_data);
+    //   await browser.close();
+    // })();
+
+    // url = URL_data[5].url;
+    // console.log(url);
+    // (async () => {
+    //   const browser = await puppeteer.launch();
+    //   const page = await browser.newPage();
+    //   await page.goto(url);
+    //   //   await page.screenshot({path: 'example.png'});
+
+    //   var news_data = await page.evaluate(() => ({
+    //     details: document.querySelector("h1").innerText
+    //   })
+    //   )
+    //   console.log(news_data);
+    //   await browser.close();
+    // })();
+
+    //  -------------------------------------------------------      
+
+    var arr = [];
+
+    function doA(i) {
+      return new Promise((resolve) => {
+        url = URL_data[i].url;
+        console.log(url);
+        (async () => {
+          const browser = await puppeteer.launch();
+          const page = await browser.newPage();
+          await page.goto(url);
+          //   await page.screenshot({path: 'example.png'});
+
+          var news_data = await page.evaluate(() => ({
+            details: document.querySelector("h1").innerText
+          })
+          )
+          console.log(news_data);
+          arr[i] = news_data;
+          await browser.close();
+        })();
+
+        setTimeout(() => {
+          resolve(arr);
+        }, 10000);
+      });
+
+      // console.log("hello");
+    }
+
+
+
+
+
+    async function main() {
+      for (var i = 0; i < URL_data.length; i++) {
+        const result = await doA(i);
+      }
+
+      console.log(arr.length);
+
+    }
+
+    main();
+
+
+
+
+    //  -------------------------------------------------------      
+
+
+    // console.log(URL_data);
+
     await browser.close();
   })();
-
-
-
 });
 
 app.listen(3000, function () {
